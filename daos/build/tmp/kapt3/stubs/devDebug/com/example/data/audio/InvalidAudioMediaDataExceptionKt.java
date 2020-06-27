2da47c7d69a734b461360c1f19ec9b6c4745ffca
Abdul Basit Malik <malikbasit12345@gmail.com>
2020-06-27 14:22:57 +0500

    git ignored daos build folder

--- daos/build/tmp/kapt3/stubs/devDebug/com/example/data/audio/InvalidAudioMediaDataExceptionKt.java
+++
@@ -1,9 +0,0 @@
-package com.example.data.audio;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 2, d1 = {"\u0000\n\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\"\u000e\u0010\u0002\u001a\u00020\u0001X\u0082T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0003"}, d2 = {"MESSAGE", "", "PROPERTY_PLACEHOLDER", "daos_devDebug"})
-public final class InvalidAudioMediaDataExceptionKt {
-    private static final java.lang.String PROPERTY_PLACEHOLDER = "[PROPERTY]";
-    private static final java.lang.String MESSAGE = "Invalid Audio media data with property [PROPERTY] to parse into Service media data";
-}
